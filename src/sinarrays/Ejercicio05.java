59f4d16262f4b3166aefdd10e99e4221566a1331
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-01-10 09:53:10 +0100

    Mejorado

--- src/sinarrays/Ejercicio05.java
+++ src/sinarrays/Ejercicio05.java
@@ -14,10 +14,10 @@ public class Ejercicio05 {
 		double sumTabla = 0;
 
 		// Declaramos la variable del número máximo de la tabla
-		double maxTabla = 0;
+		double maxTabla;
 
 		// Declaramos la variable del número mínimo de la tabla
-		double minTabla = 0;
+		double minTabla;
 
 		// Creamos nuestra tabla de longitud 10
 		double tabla[] = new double[10];
@@ -36,24 +36,24 @@ public class Ejercicio05 {
 			numPedido = sc.nextDouble();
 			// Y lo asignamos a la tabla en la posición correspondiente
 			tabla[i] = numPedido;
-			// sumTabla será la suma de cada uno de los elementos de la tabla
-			sumTabla += tabla[i];
-			
-			// Si estamos en la primera posición
-			if (i == 0) {
-				// Asinamos como máximo y como mínimo al primer elemento de la tabla
-				maxTabla = tabla[i];
-				minTabla = tabla[i];
-				// Si estamos en las demás posiciones
-			} else {
-				// Si el número es mayor que el asignado anteriormente se asigna como máximo al
-				// siguiente, si no, se conserva el anterior
-				maxTabla = tabla[i] > maxTabla ? tabla[i] : maxTabla;
-				// Si el número es menor que el asignado anteriormente se asigna como mínimo al
-				// siguiente, si no, se conserva el anterior
-				minTabla = tabla[i] < minTabla ? tabla[i] : minTabla;
-			}
+		}
+		
+		// Asignamos al máximo y al mínimo el primer número de la tabla
+		maxTabla = tabla[0];
+		minTabla = tabla[0];
 
+		// Bucle for que recorrerá toda la tabla
+		for (double valor : tabla) {
+
+			// sumTabla será la suma de cada uno de los elementos de la tabla
+			sumTabla += valor;
+
+			// Si el número es mayor que el asignado anteriormente se asigna como máximo al
+			// siguiente, si no, se conserva el anterior
+			maxTabla = valor > maxTabla ? valor : maxTabla;
+			// Si el número es menor que el asignado anteriormente se asigna como mínimo al
+			// siguiente, si no, se conserva el anterior
+			minTabla = valor < minTabla ? valor : minTabla;
 		}
 
 		for (double valor : tabla) {
