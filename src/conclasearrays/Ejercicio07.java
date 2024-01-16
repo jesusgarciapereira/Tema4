8c3accb0bcc72c78552bc9f6917d1921e267d09e
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-01-16 14:25:11 +0100

    Intento de no repetir números

--- src/conclasearrays/Ejercicio07.java
+++ src/conclasearrays/Ejercicio07.java
@@ -13,22 +13,25 @@ public class Ejercicio07 {
 		int apuesta[] = new int[6];
 
 		// Tabla con la combinación ganadora
-		int combinacionGanadora[] = new int[6];
+		int combinacionGanadora[] = {8,13,20,29,41,43};
 
 		// Bucle for que recorerá toda la tabla de nuestra apuesta
 		for (int i = 0; i < apuesta.length; i++) {
 			// A cada elemento de la tabla se le asigna un número aleatorio entre 1 y 49
 			apuesta[i] = (int) (Math.random() * 49 + 1);
-		}
-
-		// Bucle for que recorerá toda la tabla de la combinación ganadora
-		for (int i = 0; i < combinacionGanadora.length; i++) {
-			// A cada elemento de la tabla se le asigna un número aleatorio entre 1 y 49
-			combinacionGanadora[i] = (int) (Math.random() * 49 + 1);
+			// Bucle for que recorerá sólo los elementos ya asignados
+				for (int j = 0; j < i; j++) {
+					// Mientras el aleatorio asignado coincida con un elemento de la tabla
+					while(apuesta[i] == apuesta [j]) {
+						// Le asignamos otro aleatorio
+						apuesta[i] = (int) (Math.random() * 49 + 1);	
+					}
+				}
 		}
 		
 		// Ordenamos la tabla de la combinación ganadora
 		Arrays.sort(combinacionGanadora);
+		Arrays.sort(apuesta);
 
 		// Bucle for-each que consultará toda la tabla de la combinación ganadora
 		for (int valorGanador : combinacionGanadora) {
