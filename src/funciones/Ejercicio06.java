123f2d9d835307121ea5fae8cff690224d51c700
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-01-18 14:40:20 +0100

    En proceso

---
+++ src/funciones/Ejercicio06.java
@@ -0,0 +1,29 @@
+package funciones;
+
+import java.util.Iterator;
+
+public class Ejercicio06 {
+
+	// ENTRADA: Una tabla y el número de elementos consecutivos de la tabla que se
+	// sumarán
+	// SALIDA: Una tabla nueva con los resultados de cada suma realizada
+	public static int[] suma(int t[], int numElementos) {
+		// Creamos la tabla sumas, cuya longitud será igual a la de la tabla del
+		// parámetro menos el número del otro parámetro más 1
+		int sumas[] = new int[t.length - numElementos + 1];
+
+		for (int i = 0; i < sumas.length; i++) {
+			
+		}
+		
+		
+		// Devuelve la tabla sumas
+		return sumas;
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
