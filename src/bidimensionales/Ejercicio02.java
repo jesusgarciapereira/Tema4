7427cb56d4367f24abc43c29aec62e51ab944327
Jesús <jesus.garciap@iesnervion.es>
2024-01-26 21:45:21 +0100

    Ejercicio 02 Hecho

--- src/bidimensionales/Ejercicio02.java
+++ src/bidimensionales/Ejercicio02.java
@@ -10,41 +10,79 @@ public class Ejercicio02 {
 		int cantAlumnos = 4;
 		// Número de notas: 5
 		int cantNotas = 5;
-		
-		// Nota mínima, nota máxima y nota media que calcularemos
+
+		// Nota mínima, nota máxima, suma de notas y nota media que calcularemos
 		int notaMin;
 		int notaMax;
-		int notaMedia;
-		
+		int sumaNotas;
+		double notaMedia;
+
 		// Creamos una tabla bidimensional para los alumnos y sus notas
 		int notasAlumnos[][] = new int[cantAlumnos][cantNotas];
 
 		// Actibamos el Scanner
 		Scanner sc = new Scanner(System.in);
 
+		// Asignamos los valores de notasAlumnos
 		// Bucle for que recorre las filas (alumnos)
-		for (int i = 1; i < cantAlumnos + 1; i++) {
+		for (int i = 0; i < cantAlumnos; i++) {
 			// Bucle for que recorre las columnas (notas)
-			for (int j = 1; j < cantNotas + 1; j++) {
-				System.out.println("Introduzca la nota " + j + " del Alumno " + i);
+			for (int j = 0; j < cantNotas; j++) {
+				// Le pedimos al usuario la nota j del alumno i (quiero que se muestren en la
+				// petición las posiciones + 1 )
+				System.out.println("Introduzca la nota " + (j + 1) + " del Alumno " + (i + 1));
+				// Y asignamos el número dado en la tabla
 				notasAlumnos[i][j] = sc.nextInt();
 			}
+			// Salto de línea al rellenar las notas de un alumno
+			System.out.println();
 		}
-		// Bucle for que recorre cada fila
+
+		// Enseñamos la tabla de las notas de los alumnos
+		// Primera fila con el encabezado "Notas"
+		System.out.println("\t\t\t  Notas");
+		// Bucle for que recorre cada fila de la tabla
 		for (int i = 0; i < cantAlumnos; i++) {
-			// Aplicamos el método sort() en cada fila
-			Arrays.sort(notasAlumnos[i]);
-			notaMin = notasAlumnos[i][0];
-			notaMax = notasAlumnos[i][cantNotas];
-		}
-		// For-each que recorre cada fila de la tabla
-		for (int[] fila : notasAlumnos) {
-			// For-each que recorre cada elemento de la fila
-			for (int valor : fila) {
-				// Muestra el valor del elemento seguido de un tabulador
-				System.out.print(valor + "\t");
+			// Muestra el nombre del alumno seguido de un tabulador
+			System.out.print("'Alumno " + (i + 1) + "'\t");
+			// Bucle for que recorre cada elemento de la fila
+			for (int j = 0; j < cantNotas; j++) {
+				// Muestra el elemento de la tabla notasAlumnos seguido de un tabulador
+				System.out.print(notasAlumnos[i][j] + "\t");
 			}
 			// Salto de línea
+			System.out.println();
+		}
+
+		// Salto de línea
+		System.out.println();
+
+		// Bucle for que recorre cada fila (alumnos)
+		for (int i = 0; i < cantAlumnos; i++) {
+			// En cada iteración del bucle, inicializamos las variables
+			notaMin = -1;
+			notaMax = -1;
+			sumaNotas = 0;
+			notaMedia = -1;
+			// Aplicamos el método sort() para ordenar cada fila
+			Arrays.sort(notasAlumnos[i]);
+			// Asignamos a la nota mínima el valor de la primera posición de la fila
+			notaMin = notasAlumnos[i][0];
+			// Asignamos a la nota máxima el valor de la última posición de la fila
+			notaMax = notasAlumnos[i][cantNotas - 1];
+			// Bucle for que recorre cada columna (notas)
+			for (int j = 0; j < cantNotas; j++) {
+				// Asignamos a la nota media la suma de todas las notas de un alumno
+				sumaNotas += notasAlumnos[i][j];
+			}
+			// Calculamos la nota media del alumno (lo casteamos para que muestre decimales)
+			notaMedia = (double) sumaNotas / cantNotas;
+
+			// Y mostramos los resultados de cada alumno
+			System.out.println("La nota más baja del Alumno " + (i + 1) + " es: " + notaMin);
+			System.out.println("La nota más alta del Alumno " + (i + 1) + " es: " + notaMax);
+			System.out.println("La nota media del Alumno " + (i + 1) + " es: " + notaMedia);
+			// Salto de línea para separar los resultados de cada alumno
 			System.out.println();
 		}
 
