219550c795c89084c9972f1fa5424fe69745447a
Jesús <jesus.garciap@iesnervion.es>
2024-01-27 11:44:08 +0100

    Ejercicio04 Hecho

---
+++ src/bidimensionales/Ejercicio04.java
@@ -0,0 +1,43 @@
+package bidimensionales;
+
+public class Ejercicio04 {
+
+	// ENTRADA: Nada
+	// SALIDA: La tablas tablas de multiplicar del 1 al 10
+	public static int[][] tablasHastaEl10() {
+		// Creamos una tabla bidimensional 10x10
+		int tablasHastaEl10[][] = new int[10][10];
+
+		// Asignamos los valores a la tabla
+		// Bucle for que recorre las filas de la tabla
+		for (int i = 0; i < tablasHastaEl10.length; i++) {
+			// Bucle for que recorre las columnas de cada fila
+			for (int j = 0; j < tablasHastaEl10[i].length; j++) {
+				// Asignamos a cada elemento el producto de su posición más 1
+				tablasHastaEl10[i][j] = (i + 1) * (j + 1);
+			}
+		}
+		// Devolvemos la tabla creada
+		return tablasHastaEl10;
+	}
+
+	public static void main(String[] args) {
+
+		// Asignamos la tabla llamada en la función que hemos creado
+		int tablasHastaEl10[][] = Ejercicio04.tablasHastaEl10();
+
+		// Mostramos la tabla a nuestro gusto
+		// Bucle for que recorre las filas de la tabla
+		for (int i = 0; i < tablasHastaEl10.length; i++) {
+			// Bucle for que recorre las columnas de cada fila
+			for (int j = 0; j < tablasHastaEl10[i].length; j++) {
+				// Mostramos cada operación de multiplicar junto con cada elemento de la tabla
+				System.out.print((i + 1) + " * " + (j + 1) + " = " + tablasHastaEl10[i][j] + "\t");
+			}
+			// Salto de línea para cada fila (cada tabla de multiplicar)
+			System.out.println();
+		}
+
+	}
+
+}
